2251454d14f93c4d38f44974df3e8181e9a8d43d
Andy Braga <ab@luckypike.com>
2018-12-01 13:42:53 +0300

    Добавлены основные блоки

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className={styles.root}>
+        <header className={styles.header}>
+          <div className={styles.logo} />
         </header>
+
+        <div className={styles.content}>
+          <div className={styles.title}>
+            Заседание Координационного совета по промышленности
+          </div>
+
+          <div className={styles.desc}>
+            Под председательством Министра промышленности и торговли РФ Д.В. Мантурова
+          </div>
+
+          <div className={styles.date}>
+            10-11 декабря 2018 г.
+          </div>
+
+          <div className={styles.where}>
+            Нижний Новгород
+          </div>
+
+          <div className={styles.registration}>
+            <a href="https://timepad.ru" className={styles.button}>
+              Зарегистрироваться
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
