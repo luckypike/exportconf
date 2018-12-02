447aea915d79cf0d442d7701c9ea76e7d008ab54
Andy Braga <ab@luckypike.com>
2018-12-03 01:58:30 +0300

    Перенесено на Rails

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import update from 'immutability-helper';
+
+import logo from '../images/logo.svg';
+import styles from './Index.module.css';
+
+const sales = [
+  {
+    id: 1,
+    name: 'QQQQ',
+  },
+  {
+    id: 2,
+    name: 'RRR',
+  },
+];
+
+
+class Index extends Component {
+  state = {
+    start: false,
+    values: {
+      name: '',
+      surname: '',
+      middlename: '',
+      phone: '',
+      email: '',
+      hotel: '',
+      sales: sales.reduce((o, v) => ({...o, [v.id]: false}), {}),
+    }
+  }
+
+  handleInputChange = (e) => {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState(state => ({
+      values: { ...state.values, [name]: value }
+    }));
+  }
+
+  handleCheckboxChange = (e) => {
+    const target = e.target;
+    const value = target.checked;
+    const name = parseInt(target.value);
+
+    const sales = update(this.state.values.sales, {
+      [name]: { $set: value }
+    });
+
+    this.setState(state => ({
+      values: { ...state.values, sales }
+    }));
+  }
+
+  _canSubmit() {
+    return (
+      this.state.values.email &&
+      this.state.values.hotel &&
+      this.state.values.phone &&
+      this.state.values.name &&
+      this.state.values.surname &&
+      this.state.values.middlename);
+  }
+
+  render() {
+    const { start, values } = this.state;
+
+    return (
+      <div className={styles.root}>
+        <header className={styles.header}>
+          <div className={styles.logo} />
+        </header>
+
+        <div className={styles.content}>
+          <div className={styles.what}>
+            Координационный совет
+          </div>
+
+          <div className={styles.title}>
+            Развитие промышленного экспорта и экспортные программы
+          </div>
+
+          <div className={styles.desc}>
+            Под председательством Министра промышленности и торговли РФ Д.В. Мантурова
+          </div>
+
+          <div className={styles.date}>
+            10-11 декабря 2018 г.
+          </div>
+
+          <div className={styles.where}>
+            Нижний Новгород
+          </div>
+
+          {!start &&
+            <div className={styles.registration}>
+              <span className={styles.button} onClick={() => this.setState({ start: true })}>
+                Регистрация
+              </span>
+            </div>
+          }
+
+          {start &&
+            <div className={classNames(styles.form, { [styles.start]: start })}>
+              <form className={styles.form}>
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Имя
+                  </div>
+                  <div className={styles.formInput}>
+                    <input type="text" name="name" value={values.name} onChange={this.handleInputChange} />
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Фамилия
+                  </div>
+                  <div className={styles.formInput}>
+                    <input type="text" name="surname" value={values.surname} onChange={this.handleInputChange} />
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Отчество
+                  </div>
+                  <div className={styles.formInput}>
+                    <input type="text" name="middlename" value={values.middlename} onChange={this.handleInputChange} />
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Электронная почта
+                  </div>
+                  <div className={styles.formInput}>
+                    <input type="text" name="email" value={values.email} onChange={this.handleInputChange} />
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Контактный телефон
+                  </div>
+                  <div className={styles.formInput}>
+                    <input type="text" name="phone" value={values.phone} onChange={this.handleInputChange} />
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  <div className={styles.formLabel}>
+                    Отель
+                  </div>
+                  <div className={styles.formInput}>
+                    <div className={styles.radio}>
+                      <label>
+                        <input name="hotel" type="radio" checked={'courtyard' == values.hotel} value="courtyard" onChange={this.handleInputChange} />
+                        Courtyard by Marriott
+                      </label>
+                    </div>
+
+                    <div className={styles.radio}>
+                      <label>
+                        <input name="hotel" type="radio" checked={'sheraton' == values.hotel} value="sheraton" onChange={this.handleInputChange} />
+                        Sheraton Kremlin
+                      </label>
+                    </div>
+
+                    <div className={styles.radio}>
+                      <label>
+                        <input name="hotel" type="radio" checked={'none' == values.hotel} value="none" onChange={this.handleInputChange} />
+                        Не требуется
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className={styles.formRow}>
+                  Список торговых представителей
+
+                  <div className={styles.checkbox}>
+                    {sales.map(sale =>
+                      <label key={sale.id}>
+                        <input type="checkbox" name="sales" onChange={this.handleCheckboxChange} checked={values.sales[sale.id]} value={sale.id} />
+                        {sale.name}
+                      </label>
+                    )}
+                  </div>
+                </div>
+
+                <div className={styles.submit}>
+                  <input type="submit" value="Зарегистрироваться" className={styles.button} disabled={!this._canSubmit()} />
+                </div>
+              </form>
+            </div>
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
